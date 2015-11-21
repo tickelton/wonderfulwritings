4753d3a2e6462867a7dd42015519916c8452a03c
tickelton <tickelton@gmail.com>
2015-11-21 15:47:45 +0100

    Cleanup

--- playground/gutenparse.py
+++ playground/gutenparse.py
@@ -1,23 +1,21 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
 import sys
 import random
 import xml.etree.ElementTree as ET
 
-# TODO:
-#   o random number 1 - 50512
-#   o get title
-#   o get author
-#   o get gutenberg link (BASEURL + "pgterms:ebook rdf:about")
 
 GUTENBERG_BASE_USL = 'http://www.gutenberg.org/ebooks/'
+
 
 def get_random_id():
     random.seed()
     return random.randint(1, 50512)
 
-def file_from_id(id):
-    return "contrib/cache/epub/{}/pg{}.rdf".format(id, id)
+
+def file_from_id(bookid):
+    return "contrib/cache/epub/{}/pg{}.rdf".format(bookid, bookid)
+
 
 def get_bookdata_from_xml(root):
     ret = {
@@ -32,39 +30,38 @@ def get_bookdata_from_xml(root):
 
     for ebook in root.findall('pgterms:ebook', ns):
         for title in ebook.findall('dcterms:title', ns):
-            ret['title'] +=  title.text
+            ret['title'] += title.text
         for edt in ebook.findall('marcrel:edt', ns):
+            # TODO: author's names can also be contained in other sections
             for agent in edt.findall('pgterms:agent', ns):
                 for name in agent.findall('pgterms:name', ns):
-                    print "foo"
-                    print name.text
                     ret['author'] += name.text
 
     return ret
 
+
 def main():
     ret = 0
 
-    id = get_random_id()
+    bookid = get_random_id()
 
-    print "id=\"{}\"".format(id)
+    print "bookid=\"{}\"".format(bookid)
 
-    file = file_from_id(id)
+    rdf_file = file_from_id(bookid)
 
-    tree = ET.parse(file)
+    tree = ET.parse(rdf_file)
     root = tree.getroot()
     bookdata = get_bookdata_from_xml(root)
 
-    print bookdata
-    #print "title=\"{}\"\n author=\"{}\"".format(bookdata['title'], bookdata{'author'})
+    print "title=\"{}\"\n author=\"{}\"".format(
+        bookdata['title'], bookdata['author']
+    )
 
-    url = GUTENBERG_BASE_USL + '{}'.format(id)
+    url = GUTENBERG_BASE_USL + '{}'.format(bookid)
 
     print "url=\"{}\"".format(url)
 
     sys.exit(ret)
-
-
 
 
 if __name__ == '__main__':
