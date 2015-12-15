23620c2503b44cd8282bb20c924f9794e9b171e9
tickelton <tickelton@gmail.com>
2015-12-15 19:19:22 +0100

    create SQLite DB with gutenberg data

--- twot.py
+++ twot.py
@@ -2,9 +2,11 @@
 
 import os
 import sys
+import sqlite3
 import xml.etree.ElementTree as ET
 
 GUTENBERG_BASE_USL = 'http://www.gutenberg.org/ebooks/'
+
 
 def file_from_id(bookid):
     return "contrib/cache/epub/{}/pg{}.rdf".format(bookid, bookid)
@@ -33,6 +35,11 @@ def get_bookdata_from_xml(root):
                 for agent in edt.findall('pgterms:agent', ns):
                     for name in agent.findall('pgterms:name', ns):
                         ret['author'] += name.text
+        if not ret['author']:
+            for edt in ebook.findall('marcrel:adp', ns):
+                for agent in edt.findall('pgterms:agent', ns):
+                    for name in agent.findall('pgterms:name', ns):
+                        ret['author'] += name.text
     if not ret['author']:
         for agent in root.findall('pgterms:agent', ns):
             for name in agent.findall('pgterms:name', ns):
@@ -40,22 +47,25 @@ def get_bookdata_from_xml(root):
 
     return ret
 
+
 def get_gutenberg_ids():
     (_, dirnames, _) = os.walk('contrib/cache/epub/').next()
     return dirnames
 
+
 def get_max_id(ids):
     maxid = 0
 
-    for id in ids:
-        if int(id) > maxid:
-            maxid = int(id)
+    for bookid in ids:
+        if int(bookid) > maxid:
+            maxid = int(bookid)
 
     return maxid
 
+
 def main(argv):
     ret = 0
-    bookid = 0
+    bookid = 1
 
     gtb_ids = get_gutenberg_ids()
     max_id = get_max_id(gtb_ids)
@@ -64,17 +74,17 @@ def main(argv):
 
     c = conn.cursor()
 
-    while(True):
+    while True:
         if bookid > max_id:
             break
         if not "{}".format(bookid) in gtb_ids:
             bookid += 1
             continue
 
-        #print "bookid=\"{}\"".format(bookid)
+        # print "bookid=\"{}\"".format(bookid)
 
         rdf_file = file_from_id(bookid)
-        #print rdf_file
+        # print rdf_file
         if os.access(rdf_file, os.F_OK):
             tree = ET.parse(rdf_file)
             root = tree.getroot()
@@ -86,19 +96,19 @@ def main(argv):
             # )
 
             c.execute(
-"""
-INSERT INTO "twot_data" VALUES (
-
+                """
+INSERT INTO "twot_data" (gtb_id, title, author) VALUES (
+  ?,?,?
 )
-"""
+""", (bookid, bookdata['title'], bookdata['author'])
             )
 
             url = GUTENBERG_BASE_USL + '{}'.format(bookid)
 
-            #print "url=\"{}\"".format(url)
+            # print "url=\"{}\"".format(url)
         else:
             pass
-            #print "{} no such file or directory.".format(rdf_file)
+            # print "{} no such file or directory.".format(rdf_file)
 
         conn.commit()
 
